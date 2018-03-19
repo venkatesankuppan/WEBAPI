5ba829f93789f0226b7fa58f4d043d83091ebc0c
Venkatesan kuppan <Venkatesan.kuppan@gisbizdev.GISBIZ-CHENNAI.COM>
2018-03-19 11:08:21 +0530

    first commit

---
+++ WebApiSampleApplication/Controllers/EmployeeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiSampleApplication.Models;
+
+namespace WebApiSampleApplication.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        IList < Employee > employees = new List < Employee > ()  
+        {  
+            new Employee()  
+                {  
+                    EmployeeId = 1, EmployeeName = "Mukesh Kumar", Address = "New Delhi", Department = "IT"  
+                },  
+                new Employee()  
+                {  
+                    EmployeeId = 2, EmployeeName = "Banky Chamber", Address = "London", Department = "HR"  
+                },  
+                new Employee()  
+                {  
+                    EmployeeId = 3, EmployeeName = "Rahul Rathor", Address = "Laxmi Nagar", Department = "IT"  
+                },  
+                new Employee()  
+                {  
+                    EmployeeId = 4, EmployeeName = "YaduVeer Singh", Address = "Goa", Department = "Sales"  
+                },  
+                new Employee()  
+                {  
+                    EmployeeId = 5, EmployeeName = "Manish Sharma", Address = "New Delhi", Department = "HR"  
+                },  
+        };
+
+        public IList<Employee> GetAllEmployees()
+        {
+            //Return list of all employees  
+            return employees;
+        }
+        public Employee GetEmployeeDetails(int id)
+        {
+            //Return a single employee detail  
+            var employee = employees.FirstOrDefault(e => e.EmployeeId == id);
+            if (employee == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+            return employee;
+        }
+        
+    }
+}
---
+++ WebApiSampleApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApiSampleApplication.WebApiApplication" Language="C#" %>
